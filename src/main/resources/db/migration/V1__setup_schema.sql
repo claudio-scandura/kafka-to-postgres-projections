9ab6b9cb675afb672ac6ae1b1ec8c1d88031559b
Claudio Scandura <cl.scandura@gmail.com>
2021-02-25 08:23:53 +0100

    Add comments to ddl.

--- src/main/resources/db/migration/V1__setup_schema.sql
+++ src/main/resources/db/migration/V1__setup_schema.sql
@@ -1,3 +1,4 @@
+-- The offset store as required and defined by Akka Projection JDBC
 CREATE TABLE IF NOT EXISTS akka_projection_offset_store (
     projection_name VARCHAR(255) NOT NULL,
     projection_key VARCHAR(255) NOT NULL,
@@ -10,6 +11,8 @@ CREATE TABLE IF NOT EXISTS akka_projection_offset_store (
 
 CREATE INDEX IF NOT EXISTS projection_name_index ON akka_projection_offset_store (projection_name);
 
+-- The table that tracks for each radar the total number of observations
+-- and the sum of the reported speed in meters per second
 create TABLE IF NOT EXISTS STATS_BY_RADAR (
     RADAR_ID VARCHAR(6) NOT NULL,
     SUM_METERS_PER_SEC BIGINT NOT NULL,
@@ -17,6 +20,8 @@ create TABLE IF NOT EXISTS STATS_BY_RADAR (
     PRIMARY KEY (RADAR_ID)
 );
 
+-- The table that tracks for each license plate the maximum observed speed
+-- of the respective vehicle and the last radar it was seen
 create TABLE IF NOT EXISTS STATS_BY_LICENSE_PLATE (
     LICENSE_PLATE VARCHAR(8) NOT NULL,
     MAX_METERS_PER_SEC BIGINT NOT NULL,
